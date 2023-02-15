9d172e211639c4c5efd01620855c00ee92fea8f3
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-02-15 21:15:50 +0530

    add new view

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        {/*  padding for inside space */}
+        <Text style={{margin:16,borderWidth:2,borderColor:'red', padding:16}}>Hello World</Text>
+      </View>
+      <Button title='tap me'/>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
   container: {
