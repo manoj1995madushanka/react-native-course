74b42272b52e5490b65c1885d1043c32e2eb0448
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-02-15 21:53:23 +0530

    change styles

--- App.js
+++ App.js
@@ -7,13 +7,15 @@ export default function App() {
       <Text>Open up App.js to start working on your app!</Text>
       <View>
         {/*  padding for inside space */}
-        <Text style={{margin:16,borderWidth:2,borderColor:'red', padding:16}}>Hello World</Text>
+        <Text style={styles.dummyText}>Hello World</Text>
       </View>
       <Button title='tap me'/>
     </View>
   );
 } 
 
+// we use StyleSheet because
+// besides auto-completeion, the stylesheet object also provides validations
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -21,4 +23,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  dummyText: {
+    margin:16,
+    padding:16,
+    borderWidth: 2,
+    borderColor: 'red',
+  }
 });
