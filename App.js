3bd8d15dfacad068937058cf16184b7305ccfdb7
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-02-22 16:07:48 +0530

    fix styles

--- App.js
+++ App.js
@@ -4,11 +4,11 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appContainer}>
-      <View style={styles.flexDirection}>
+      <View style={styles.inputContainer}>
         <TextInput style={styles.textInput} placeholder='your course goal!'/>
         <Button title='Add Goal'/>
       </View>
-    <View>
+    <View style={styles.goalsContainer}>
       <Text>List of goals..</Text>
     </View>
     </View>
@@ -19,17 +19,27 @@ export default function App() {
 // besides auto-completeion, the stylesheet object also provides validations
 const styles = StyleSheet.create({
   appContainer: {
+    flex:1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex:1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom:24,
+    borderBottomWidth:1,
+    borderBottomColor:'#cccccc',
   },
   textInput: {
     borderColor: '#cccccc',
     borderWidth: 1,
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding:8,
+  },
+  goalsContainer: {
+    flex: 5
   }
 });
