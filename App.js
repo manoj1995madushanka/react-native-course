b1fa66530654cc085b50a01b627031ca846293fb
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-02-23 23:05:33 +0530

    fix styles for both ios and android

--- App.js
+++ App.js
@@ -29,7 +29,13 @@ export default function App() {
         <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <Text key={goal} style={styles.goalItem}>{goal}</Text>)}
+        {courseGoals.map((goal) =>
+        (
+          <View key={goal} style={styles.goalItem}>
+            <Text style={styles.goalText}>{goal}</Text>
+          </View>
+        )
+        )}
       </View>
     </View>
   );
@@ -66,7 +72,9 @@ const styles = StyleSheet.create({
     margin: 8,
     padding: 8,
     borderRadius: 6,
-    backgroundColor: '#5e0acc',
+    backgroundColor: '#5e0acc'
+  },
+  goalText: {
     color: 'white'
   }
 });
