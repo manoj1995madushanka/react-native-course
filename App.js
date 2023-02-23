3b3f49ecd8d4d0c2eb189ca191b4431ddfed75d4
manoj1995madushanka <madushankamanoj414@gmail.com>
2023-02-23 21:22:53 +0530

    add note

--- App.js
+++ App.js
@@ -4,55 +4,60 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
-  const [enteredGoalText,setEnteredGoalText] = useState('');
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
 
-  function goalInputHandler(enteredText){
+  function goalInputHandler(enteredText) {
     //console.log(enteredText);
     setEnteredGoalText(enteredText);
   };
 
-  function addGoalHandler(){
-    console.log(enteredGoalText);
+  function addGoalHandler() {
+    // console.log(enteredGoalText);
+    //setCourseGoals([...courseGoals,enteredGoalText]);
+    // above add goal to existing list courseGoals depend on previous courseGoals. In react we has better option
+    // to do the same task
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, enteredGoalText]);
   };
 
 
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='your course goal!' 
-        onChangeText={goalInputHandler}/>
-        <Button title='Add Goal' onPress={addGoalHandler}/>
+        <TextInput style={styles.textInput} placeholder='your course goal!'
+          onChangeText={goalInputHandler} />
+        <Button title='Add Goal' onPress={addGoalHandler} />
       </View>
-    <View style={styles.goalsContainer}>
-      <Text>List of goals..</Text>
-    </View>
+      <View style={styles.goalsContainer}>
+        {courseGoals.map((goal) => <Text>{goal}</Text>)}
+      </View>
     </View>
   );
-} 
+}
 
 // we use StyleSheet because
 // besides auto-completeion, the stylesheet object also provides validations
 const styles = StyleSheet.create({
   appContainer: {
-    flex:1,
+    flex: 1,
     padding: 50,
     paddingHorizontal: 16,
   },
   inputContainer: {
-    flex:1,
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom:24,
-    borderBottomWidth:1,
-    borderBottomColor:'#cccccc',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderColor: '#cccccc',
     borderWidth: 1,
     width: '70%',
     marginRight: 8,
-    padding:8,
+    padding: 8,
   },
   goalsContainer: {
     flex: 5
